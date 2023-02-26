6c6ab4c537301fd312162f798157121c721288f2
Benedikt Korbach <benedikt.korbach@hotmail.de>
2023-02-26 13:31:29 +0100

    added counter

--- hanoi.py
+++ hanoi.py
@@ -4,13 +4,18 @@ Created on Mon Feb 20 08:24:20 2023
 
 @author: Benedikt Korbach
 """
+# Iniate total_moves counter
+total_moves = 0
 
+# Define moving method
 def move(n, from_rod, to_rod):
     print("Move plate", n, "from rod", from_rod, "to", to_rod)
+    global total_moves
+    total_moves += 1
 
 def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
 
-    # base case
+    # Base case
     if n == 0:
         return
 
@@ -25,3 +30,6 @@ def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
 
 # Select the number of plates by changing the first variable
 TowerOfHanoi(3, 'A', 'C', 'B')
+
+# Print total moves needed to solve Tower of Hanoi puzzle
+print("Total moves needed to solve: ", total_moves)
