1f03c54f5cf7d4e22f3181626623c449103d272d
Benedikt Korbach <benedikt.korbach@hotmail.de>
2023-02-26 13:21:14 +0100

    finalized

--- hanoi.py
+++ hanoi.py
@@ -2,13 +2,26 @@
 """
 Created on Mon Feb 20 08:24:20 2023
 
-@author: Hannah
+@author: Benedikt Korbach
 """
 
-def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
-    
-    #delete the pass and replace with your recursive algorithm
-    pass
+def move(n, from_rod, to_rod):
+    print("Move plate", n, "from rod", from_rod, "to", to_rod)
 
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
 
-TowerOfHanoi(3, 'A', 'C', 'B')  
+    # base case
+    if n == 0:
+        return
+
+    # Move all plates except for the biggest one to the auxilliary rod
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+
+    # Move the biggest plate to the to-rod
+    move(n, from_rod, to_rod)
+
+    # Move all the remaining plates from the auxilliary rod to the to-rod
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+
+# Select the number of plates by changing the first variable
+TowerOfHanoi(3, 'A', 'C', 'B')
